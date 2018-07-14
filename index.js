81861cc96abab7ebc953b68376aea78c8007d97d
Norman Joyner <norman.joyner@gmail.com>
2018-07-13 22:52:53 -0400

    initial implementation

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+    retry(fn, options = {}) {
+        // set default attempts
+        if(!options._attempts) {
+            options._attempts = 1;
+        }
+
+        // set default backoff policy
+        if(!options.backoff_policy) {
+            options.backoff_policy = 'constant';
+        }
+
+        // set default backoff duration
+        if(!options.backoff_duration) {
+            options.backoff_duration = 1500;
+        }
+
+        const _retry = async (...args) => {
+            try {
+                return await fn(...args);
+            } catch(err) {
+                if(options.max_retries === undefined || options._attempts < options.max_retries) {
+                    let backoff;
+
+                    if(options.backoff_policy === 'exponential') {
+                        backoff = Math.pow((options.backoff_duration / 1000), options._attempts) * 1000;
+                    } else if(options.backoff_policy === 'linear') {
+                        backoff = options.backoff_duration * options._attempts;
+                    } else {
+                        backoff = options.backoff_duration * 1;
+                    }
+
+                    options._attempts++;
+
+                    return setTimeout(async () => {
+                        return await _retry(...args);
+                    }, backoff);
+                } else {
+                    throw err;
+                }
+            }
+        }
+
+        return async (...args) => {
+            return await _retry(...args);
+        };
+    }
+};
